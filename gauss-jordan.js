24ebb1063b96e421cf00a144e90aa56424deefa4
Murtadha <137140876+murtadha203@users.noreply.github.com>
2024-11-16 09:46:17 +0300

    Update gauss-jordan.js
    add id = dis to display table

--- gauss-jordan.js
+++ gauss-jordan.js
@@ -135,7 +135,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function displayMatrix(matrix) {
-        let html = '<table>';
+        let html = '<table id = dis>';
         matrix.forEach(row => {
             html += '<tr>';
             row.forEach(value => {
